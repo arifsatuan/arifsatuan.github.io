5dd7f109833b0c33c323f237734f612bb914f984
arifsatuan <arifsatuan@gmail.com>
2022-10-22 09:32:53 +0700

    22-Oct-2022=09:32

--- push.sh
+++ push.sh
@@ -2,13 +2,13 @@
 echo -n "Comment: "
 #read ans
 #echo $ans
-tgl ='date +"%d-%b-%Y %H:%M"'
+tgl=`date +"%d-%b-%Y=%H:%M"`
 git add .
 #comment =  $RANDOM | base64 | head -c 20;
-git commit -m tgl
+git commit -m $tgl
 git push origin main
 hugo
 cd public
 git add .
-git commit -m tgl
+git commit -m $tgl
 git push origin pages
