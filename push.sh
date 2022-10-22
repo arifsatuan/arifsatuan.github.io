3ed4dfbbc5f23f7906e9bb6a01e3517fd7ee9b77
arifsatuan <arifsatuan@gmail.com>
2022-10-22 09:19:27 +0700

    16189

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo -n "Comment: "
-read ans
-echo $ans
-git add . 
-git commit -m $ans
+#read ans
+#echo $ans
+git add .
+$comment =  $RANDOM | base64 | head -c 20;
+git commit -m  $RANDOM | base64 | head -c 20;
 git push origin main
 hugo
 cd public
